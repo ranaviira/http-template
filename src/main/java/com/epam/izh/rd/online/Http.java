dc6205b0bdae6d41193f57fbf2e062ac079aacb6
renat <84rent@gmail.com>
2021-11-12 19:33:28 +0400

    slowpoke win

--- src/main/java/com/epam/izh/rd/online/Http.java
+++ src/main/java/com/epam/izh/rd/online/Http.java
@@ -1,7 +1,18 @@
 package com.epam.izh.rd.online;
 
+import com.epam.izh.rd.online.entity.Pokemon;
+import com.epam.izh.rd.online.service.SimplePokemonFetching;
+import com.epam.izh.rd.online.service.SimplePokemonFightingClub;
+
 public class Http {
+
     public static void main(String[] args) {
 
+        SimplePokemonFightingClub simplePokemonFightingClub = new SimplePokemonFightingClub();
+
+        Pokemon winner = simplePokemonFightingClub.doBattle(new SimplePokemonFetching().fetchByName("pikachu")
+                , new SimplePokemonFetching().fetchByName("slowpoke"));
+
+        simplePokemonFightingClub.showWinner(winner);
     }
 }
